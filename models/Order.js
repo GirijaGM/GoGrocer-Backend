753f25c2e9fdf65c611bb66b951b71aa6aea2cc8
GirijaGM <girijamano1@gmail.com>
2026-05-30 20:15:27 +0530

    Initial backend commit

---
+++ models/Order.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  items: Array,
+  total: Number,
+  status: {
+    type: String,
+    default: "Active 🟢"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Order", orderSchema);
---
+++ routes/authRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+router.post("/signup", async (req, res) => {
+    console.log(req.body);
+  const { name, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  await user.save();
+
+  res.json({ message: "User registered" });
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Wrong password" });
+  }
+
+  res.json({
+    message: "Login success",
+    user
+  });
+});
+
+module.exports = router;
